e8a2d69eac1ce3ad95b25bdb40b39631c1c9f088
ethai16 <erick.thai16@gmail.com>
2019-02-07 20:09:36 -0600

    completed

--- memory-game/src/MemoryCard.js
+++ memory-game/src/MemoryCard.js
@@ -2,10 +2,21 @@ import React, {Component} from 'react';
 import './MemoryCard.css'
 
 class MemoryCard extends Component{
+
+
     render(){
+        var memoryCardInnerClass = "MemoryCardInner"
+        if (this.props.isFlipped){
+            memoryCardInnerClass += ' flipped'
+        }
         return(
-            <div className = "MemoryCard">
-                <img src= "https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"></img>
+            <div className = "MemoryCard" onClick = {this.props.pickCard}>
+                <div className = {memoryCardInnerClass}>
+                    <div className = "MemoryCardBack">
+                        <img src= "https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt ="dc"></img>
+                    </div>
+                    <div className = "MemoryCardFront">{this.props.symbol}</div>
+                </div>
             </div>
         )
     }
